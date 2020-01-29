0f3c4f6508e81637a890727267641aba4abed274
Steve Smith <onecrane@gmail.com>
2020-01-28 21:40:37 -0600

    Initial commit

---
+++ Assets/DriveControls.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class DriveControls : MonoBehaviour
+{
+    #region Properties
+
+    [Range(0f, 500f)]
+    public float maxSpeed = 10;
+
+    [Range(0f, 100f)]
+    public float accelerationRate = 1;
+
+    [Range(0f, 1f)]
+    public float frictionDecay = 0.1f;
+
+    [Range(0f, 500f)]
+    public float steerRate = 1;
+
+    [Range(0f, 50000f)]
+    public float brakeRate = 10;
+
+    public float currentSpeed { get; private set; }
+
+    #endregion
+
+    #region Utilities
+
+    float FDT { get { return Time.fixedDeltaTime; } }
+
+    #endregion
+
+    #region Controls
+
+    void FixedUpdate()
+    {
+        HandleSpeedInput();
+        HandleSteeringInput();
+    }
+
+    private void HandleSteeringInput()
+    {
+        float steerDelta = steerRate * FDT;
+
+        bool inputLeft = Input.GetKey(KeyCode.A), inputRight = Input.GetKey(KeyCode.D);
+
+        if (!(inputLeft && inputRight))
+        {
+            if (inputLeft) transform.Rotate(transform.up, -steerDelta);
+            if (inputRight) transform.Rotate(transform.up, steerDelta);
+        }
+    }
+
+    private void HandleSpeedInput()
+    {
+        bool inputForward = Input.GetKey(KeyCode.W);
+        bool inputReverse = Input.GetKey(KeyCode.S);
+
+        float brakeDelta = brakeRate * FDT, speedDelta = accelerationRate * FDT, frictionDelta = frictionDecay * FDT;
+
+        // Exclusive OR to determine if user-controlled speed change is applied
+        if (!(inputForward && inputReverse))
+        {
+            if (inputForward)
+            {
+                if (currentSpeed < 0)
+                    currentSpeed = Mathf.Clamp(currentSpeed + brakeDelta, currentSpeed, 0);
+                else
+                    currentSpeed += speedDelta;
+            }
+
+            if (inputReverse)
+            {
+                if (currentSpeed > 0)
+                    currentSpeed = Mathf.Clamp(currentSpeed - brakeDelta, 0, currentSpeed);
+                else
+                    currentSpeed -= speedDelta;
+            }
+        }
+        else
+        {
+            currentSpeed *= (1.0f - frictionDelta);
+        }
+
+        currentSpeed = Mathf.Clamp(currentSpeed, -maxSpeed, maxSpeed);
+        GetComponent<Rigidbody>().velocity = transform.forward * currentSpeed;
+    }
+
+    #endregion
+}
---
+++ Assets/Kickable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kickable : MonoBehaviour
+{
+    public float kickFactor = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Task: Kick the ball harder based on the car's current speed.
+        if (collision.gameObject.name == "Car")
+        {
+            GetComponent<Rigidbody>().AddForceAtPosition(collision.gameObject.transform.forward * collision.gameObject.GetComponent<DriveControls>().currentSpeed * kickFactor, collision.contacts[0].point);
+        }
+    }
+
+}
